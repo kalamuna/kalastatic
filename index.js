96ad2e1c257abc8ea0a6590fdc3548ace811ec1c
Rob Loach <robloach@gmail.com>
2015-07-10 15:13:16 -0400

    Add examples

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
 var Metalsmith = require('metalsmith')
 var Promise = require('promise')
+var fs = require('fs')
+var path = require('path')
+var extend = require('extend')
+
+function getConfig(base, cb) {
+  if (!base) {
+    base = '.'
+  }
+  var config = {plugins: []}
+  var config = {
+    plugins: [
+      {
+        'name': 'metalsmith-ignore',
+        'options': [
+          'node_modules'
+        ]
+      },
+      {
+        'name': 'metalsmith-packagejson'
+      },
+      {
+        'name': 'metalsmith-paths'
+      },
+      {
+        'name': 'metalsmith-metadata-convention'
+      },
+      {
+        'name': 'metalsmith-collections-convention'
+      },
+      {
+        'name': 'metalsmith-jstransformer'
+      },
+      {
+        'name': 'metalsmith-jstransformer-layouts'
+      },
+      {
+        'name': 'metalsmith-metadata-convention'
+      },
+      {
+        'name': 'metalsmith-ignore',
+        'options': [
+          // Ignore partials.
+          '**/_*'
+        ]
+      }
+    ]
+  }
+  var packageJson = path.join(base, 'package.json')
+  fs.readFile(packageJson, function (err, data) {
+    if (err) {
+      data = {}
+    }
+    else {
+      data = JSON.parse(data)
+    }
+    config = extend({}, data, config)
+    cb(config)
+  })
+}
 
 module.exports = function (base) {
   return new Promise(function (fulfill, reject) {
-    if (!base) {
-      base = '.'
-    }
+    getConfig(base, function (config) {
+      console.log(config)
 
-    var plugins = {
-      'metalsmith-paths': true,
-      'metalsmith-metadata-convention': {},
-      'metalsmith-collections-convention': {},
-      'metalsmith-jstransformer': {},
-      'metalsmith-jstransformer-layouts': {},
-      'metalsmith-ignore': [
-        // Ignore partials.
-        '**/_*'
-      ]
-    }
+      var metalsmith = Metalsmith(base)
 
-    var metalsmith = Metalsmith(base)
-
-    for (var pluginName in plugins) {
-      var plugin = require(pluginName)
-      metalsmith.use(plugin(plugins[pluginName]))
-    }
-
-    metalsmith.build(function (err) {
-      if (err) {
-        return reject(err)
+      for (var i in config.plugins) {
+        var pluginName = config.plugins[i].name
+        var pluginOptions = config.plugins[i].options || {}
+        var plugin = require(pluginName)
+        metalsmith.use(plugin(config.plugins[pluginName]))
       }
-      fulfill()
+
+      if (config.source) {
+        metalsmith.source(config.source)
+      }
+
+      metalsmith.build(function (err) {
+        if (err) {
+          return reject(err)
+        }
+        fulfill()
+      })
     })
+
+
   })
 }
