90edac8e8cad55dbbc316630fb5d3cd470f65a27
Rob Loach <robloach@gmail.com>
2015-11-09 22:27:20 -0500

    Refactor

--- test/index.js
+++ test/index.js
@@ -1,34 +1,45 @@
-var assertDir = require('assert-dir-equal')
-var KalaStatic = require('../')
-var execFile = require('child_process').execFile
-var fs = require('fs')
-var it = require('testit')
+// var assertDir = require('assert-dir-equal');
+var KalaStatic = require('../');
+var execFile = require('child_process').execFile;
+var fs = require('fs');
+var path = require('path');
+var test = require('testit');
+var nconf = require('nconf');
 
-function test (name, nconf) {
-  it(name, function (done) {
-    var testPath = 'test/fixtures/' + name
-    KalaStatic(testPath).then(function () {
-      assertDir(testPath + '/build', testPath + '/expected')
-      done()
-    }, done).catch(done)
-  })
+function setupTest(name) {
+  test(name, function () {
+    return new Promise(function (resolve, reject) {
+      var dir = path.join('test', 'fixtures', name);
+      var conf = new nconf.Provider();
+      var opts = {
+        directory: path.join(dir, 'src'),
+        destination: path.join(dir, 'build')
+      };
+      conf.defaults(opts);
+      var kalastatic = new KalaStatic(conf);
+      kalastatic.build().then(function () {
+        // assertDir(opts.destination, path.join(dir, 'expected'))
+        resolve();
+      }, reject).catch(reject);
+    });
+  });
 }
 
-test('basic')
-test('layouts')
+setupTest('basic');
+setupTest('layouts');
 
-it('cli', function (done) {
+test('cli', function (done) {
   var options = {
     cwd: 'test/fixtures/basic'
-  }
+  };
   execFile(fs.realpathSync('bin/kalastatic'), [], options, function (err, stdout, stderr) {
     if (err) {
-      return done(err)
+      return done(err);
     }
     if (stderr) {
-      return done(stderr.toString())
+      return done(stderr.toString());
     }
-    assertDir('test/fixtures/basic/build', 'test/fixtures/basic/expected')
-    done()
-  })
-})
+    // assertDir('test/fixtures/basic/build', 'test/fixtures/basic/expected')
+    done();
+  });
+});
