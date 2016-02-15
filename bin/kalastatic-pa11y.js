e2d3fdf52cad8ebd9ac417fa0c705ab183e69b51
Rob Loach <robloach@gmail.com>
2016-02-15 16:54:13 -0500

    EECS-200 Allow running the accessibility report on multiple pages

--- bin/kalastatic-pa11y.js
+++ bin/kalastatic-pa11y.js
@@ -1,32 +1,40 @@
 #!/usr/bin/env node
-
 'use strict';
 
-var pa11y = require('pa11y');
+// Dependencies
+var childProcess = require('child_process')
+var fs = require('fs')
+var shellEscape = require('shell-escape')
+
+// Load the pages to parse on.
 var input = JSON.parse(require('fs').readFileSync('./pa11y.json', 'utf8'));
 var pages = input.pages;
 
-// Create a test instance with some default options
-var test = pa11y({
+// Retrieve the current working directory.
+var cwd = require('process').cwd()
 
-  // Log what's happening to the console
-  log: {
-    debug: console.log.bind(console),
-    error: console.error.bind(console),
-    info: console.log.bind(console)
-  }
+// Begin the index template, sharing the CSS style from the pa11y report.
+var output = '<!doctype html><html lang="en"><head><meta charset="utf-8"><body>'
+output += '<style> html, body { margin: 0; padding: 0; background-color: #fff; font-family: Arial, Helvetica, sans-serif; font-size: 16px; line-height: 22px; color: #333; } li { margin-bottom: 15px; } h1, h2, p, pre, ul { margin-top: 0; margin-bottom: 0; } h1 { margin-bottom: 10px; font-size: 24px; line-height: 24px; } h2 { font-size: 16px; } pre { white-space: pre-wrap; overflow: auto; } .page { max-width: 800px; margin: 0 auto; padding: 25px; } .counts { margin-top: 30px; font-size: 20px; } .count { display: inline-block; padding: 5px; border-radius: 5px; border: 1px solid #eee; } .clean-list { margin-left: 0; padding-left: 0; list-style: none; } .results-list { margin-top: 30px; } .result { padding: 10px; border-radius: 5px; border: 1px solid #eee; } .error { background-color: #fdd; border-color: #ff9696; } .warning { background-color: #ffd; border-color: #e7c12b; } .notice { background-color: #eef4ff; border-color: #b6d0ff; } </style>'
+output += '<div class="page"><h1>Accessibility Reports</h1><ul>'
 
-});
+// Run the accessibility report for each page.
+for (var i in pages) {
+  // Construct the executable to run pa11y.
+  var args = ['pa11y']
+  args.push('-c', './pa11y.json')
+  args.push('-r', 'html')
+  args.push('-l', 'none')
+  args.push('file://' + cwd + '/build/' + pages[i])
+  var cmd = shellEscape(args) + ' > build/a11y/' + i + '.html'
 
-function call(url, callback) {
-  test.run(url, callback);
+  // Run the command.
+  childProcess.execSync(cmd)
+
+  // Add the report to the index.
+  output += '<li><a href="' + i + '.html">' + pages[i] + '</a></li>'
 }
 
-async.map(pages, call, function (error, results) { 
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log(results.home);
-  console.log(results.plants);
-});
-
+// Finish and save the template.
+output += '</ul></div></body></html>'
+fs.writeFileSync('build/a11y/index.html', output)
