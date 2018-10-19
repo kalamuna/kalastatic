5d7a5c00d6300ba9e320823e76a0ce02c5f4b717
Rob Loach <robloach@gmail.com>
2018-10-18 19:12:46 -0700

    Debugging

--- src/kalastatic.js
+++ src/kalastatic.js
@@ -111,6 +111,7 @@ KalaStatic.prototype.build = function () {
         metalsmith.use(mod(opts))
       }
     }
+    console.log(options)
 
     // Build the application.
     metalsmith.build(err => {
