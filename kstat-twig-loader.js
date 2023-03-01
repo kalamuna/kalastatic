025e3ee03b07db4592f5f653393b43d8ba49e514
Cole Geerts <cjgeerts@gmail.com>
2023-03-01 13:58:35 -0500

    Adding Compilation Via Twig JS

---
+++ kstat-twig-loader.js
@@ -0,0 +1,72 @@
+// This script would exist within the node module and doesn't need invoking during a real project
+import {
+  promises as fs
+} from 'fs';
+import Twig from "twig";
+
+import {
+  addDrupalExtensions
+} from 'drupal-twig-extensions/twig';
+
+addDrupalExtensions(Twig);
+
+// Finds twig pages in a directory and returns an array of filenames
+export const findTwigPages = async (directory) => {
+  let twigFiles = [];
+
+  await fs.readdir(`${directory}`)
+    .then(async (files) => {
+      for (const file of files) {
+        await fs.stat(`${directory}/${file}`)
+          .then(async (entry) => {
+            if (entry.isDirectory()) {
+              const pages = await findTwigPages(`${directory}/${file}`);
+              for (const page of pages) {
+                twigFiles.push(page);
+              }
+            } else if (file.endsWith('.twig')) {
+              twigFiles.push(`${directory}/${file}`);
+            }
+          })
+      }
+    });
+
+  return twigFiles;
+};
+
+// Compiles a twig file and returns HTML
+export const compileTwig = async (directory, twigFile) => {
+  console.log(`Compiling Twig File: ${twigFile}`);
+
+  const twigFileStream = await fs.readFile(`${twigFile}`, { encoding: 'utf8' });
+
+  const compiledTwig = Twig.twig({
+    data: twigFileStream,
+    allowInlineIncludes: true,
+    path: directory,
+    namespaces: { 'templates': 'templates', 'components': 'components' }
+  }).render();
+
+  return compiledTwig;
+};
+
+// Writes HTML to a given location
+export const writeHtml = async (path, html) => {
+  console.log(`Writing ${path}`);
+
+  fs.writeFile(path, html)
+  //fs.mkdir(path.split("/").pop().join("/"), { recursive: true });
+};
+
+// Executes the other functions of Kstat
+export const kstat = async (directory) => {
+  const pages = await findTwigPages(directory);
+
+  for (const page of pages) {
+    const compiledHtml = await compileTwig(directory, page).catch(err => console.log(err.message));
+
+    writeHtml(`build/${page.replace(`${directory}/`, "").replace(".twig", "")}`, compiledHtml);
+  }
+
+  // console.log(pages);
+};
---
+++ main.js
@@ -0,0 +1,4 @@
+// This script would exist within the node module and doesn't need invoking during a real project
+import { kstat } from "./kstat-twig-loader.js";
+
+kstat('pages');
