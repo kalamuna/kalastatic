5025ed9cf42cedfe7a16c95d2a1272a5064bfcb6
Rob Loach <robloach@gmail.com>
2023-08-04 11:19:57 -0400

    KSTAT-143: Move to kalastatic package

--- main.js
+++
@@ -1,7 +0,0 @@
-// This script would exist within the node module and doesn't need to be invoked during a real project
-
-import { readFileSync } from 'fs';
-import { kstat } from "./kstat.js";
-
-const config = JSON.parse(readFileSync('./package.json'));
-kstat(config.kalastatic);
