6c0e5aff0bef8ad619f55f3b7b56413c53785158
Rob Loach <robloach@gmail.com>
2024-07-10 15:03:17 -0400

    KSTAT-183: Add environment variables to Twig (#659)

--- src/kalastatic.js
+++ src/kalastatic.js
@@ -227,7 +227,10 @@ export const kstat = async (config) => {
   config.destination = config.destination || 'build';
 
   // Add the base url if set by the environmetn and / otherwise.
+  // TODO: The `base_url` variable will be deprecated in favor of `env.base_url`.
   renderData.base_url = process.env.base_url || "";
+  // Add all the environment variables to the `env` object in the render data.
+  renderData.env = process.env;
 
   // Delete all the destination files/directories in each source and assets so we don't get orphans.
   await clearDestination(config.destination);
